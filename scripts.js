3ae0497ca5ef02f689c9c8da87848ae5a6478450
Andrés Cardot <cardotandres@gmail.com>
2023-03-01 02:33:52 -0300

    Página responsiva lista. Códigos estilizados. Pruebas hechas.

--- scripts.js
+++ scripts.js
@@ -1,53 +1,130 @@
-// funciones particulares para colapsar bloques de info (buscar implementación universal)
-let boton_mostrar_ocultar1 = document.getElementById("mostrar_ocultar1");
-boton_mostrar_ocultar1.addEventListener("click", mostrar_ocultar1);
-let boton_mostrar_ocultar2 = document.getElementById("mostrar_ocultar2");
-boton_mostrar_ocultar2.addEventListener("click", mostrar_ocultar2);
-let boton_mostrar_ocultar3 = document.getElementById("mostrar_ocultar3");
-boton_mostrar_ocultar3.addEventListener("click", mostrar_ocultar3);
-let boton_mostrar_ocultar4 = document.getElementById("mostrar_ocultar4");
-boton_mostrar_ocultar4.addEventListener("click", mostrar_ocultar4);
+// ----- GOOGLE TRADUCTOR -----
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'es', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_traductor')
+}
 
-function mostrar_ocultar1 (){
-    let numero_info = document.getElementById("info1");
-    let mas_menos = document.getElementById("mas_menos1");
-    mostrar_ocultar_abs(numero_info, mas_menos);
+// ----- MODALES (FOTO MAXIMIZADA) -----
+// --- Header ---
+// Insertar foto dentro del modal
+let foto_header = document.getElementById("foto_header");
+let foto_grande_header = document.getElementById("foto_grande_header");
+let modal_header = document.getElementById("modal_header");
+foto_header.addEventListener("click", maximizar_header);
+
+function maximizar_header() {
+  modal_header.style.display = "block";
+  foto_grande_header.src = this.src;
 }
-function mostrar_ocultar2 (){
-    let numero_info = document.getElementById("info2");
-    let mas_menos = document.getElementById("mas_menos2");
-    mostrar_ocultar_abs(numero_info, mas_menos);
+
+// Botón de cierre y cerrado por click afuera de la foto
+let x = document.getElementsByClassName("cerrar")[0];
+x.addEventListener("click", cerrar);
+function cerrar() {
+    modal_header.style.display = "none";
 }
-function mostrar_ocultar3 (){
-    let numero_info = document.getElementById("info3");
-    let mas_menos = document.getElementById("mas_menos3");
-    mostrar_ocultar_abs(numero_info, mas_menos);
-}
-function mostrar_ocultar4 (){
-    let numero_info = document.getElementById("info4");
-    let mas_menos = document.getElementById("mas_menos4");
-    mostrar_ocultar_abs(numero_info, mas_menos);
-}
-// función absoluta para colapsar elementos
-function mostrar_ocultar_abs(numero_info, mas_menos){
-    if (numero_info.className === "blanco info"){
-        numero_info.className = "blanco info oculto";
-        mas_menos.className = "iconos icon-mas";
-    } else {
-        numero_info.className = "blanco info";
-        mas_menos.className = "iconos icon-menos";
+window.addEventListener("click", cerrar2);
+function cerrar2 (evento) {
+    if (evento.target == modal_header || evento.target == modal_datos) {
+      modal_header.style.display = "none";
+      modal_datos.style.display = "none";
     }
 }
 
-// evento click en el botón de temas, dispara las funciones
-let boton_tema = document.getElementById("switch");
+// --- Datos personales ---
+let foto_datos = document.getElementById("foto_datos");
+let foto_grande_datos = document.getElementById("foto_grande_datos");
+let modal_datos = document.getElementById("modal_datos");
+foto_datos.addEventListener("click", maximizar_datos);
+function maximizar_datos() {
+    modal_datos.style.display = "block";
+    foto_grande_datos.src = this.src;
+}
+
+// ----- MOSTRAR/OCULTAR DATOS PERSONALES -----
+let guia = true;
+let boton_menu = document.getElementById("boton_menu");
+let datos_ocultables = document.getElementById("datos_ocultables");
+boton_menu.addEventListener("click", mostrar_ocultar_menu);
+function mostrar_ocultar_menu() {
+    datos_ocultables.classList.toggle("oculto", guia);
+    guia = !guia;
+}
+
+// ----- MOSTRAR/OCULTAR BLOQUES INFORMACION PERSONAL
+// Función generalizada
+function mostrar_ocultar_info (subtitulo, icono) {
+    if (subtitulo.className === "base subtitulos"){
+        subtitulo.className = "base subtitulos oculto";
+        icono.className = "icon-mas";
+    } else {
+        subtitulo.className = "base subtitulos";
+        icono.className = "icon-menos";
+    }
+}
+
+// Funciones particulares
+let boton_info_1 = document.getElementById("boton_info_1");
+boton_info_1.addEventListener("click", mostrar_ocultar_1);
+function mostrar_ocultar_1() {
+    let subtitulos_info_1 = document.getElementById("subtitulos_info_1");
+    let icono_info_1 =document.getElementById("icono_info_1");
+    mostrar_ocultar_info(subtitulos_info_1, icono_info_1);
+}
+
+let boton_info_2 = document.getElementById("boton_info_2");
+boton_info_2.addEventListener("click", mostrar_ocultar_2);
+function mostrar_ocultar_2() {
+    let subtitulos_info_2 = document.getElementById("subtitulos_info_2");
+    let icono_info_2 =document.getElementById("icono_info_2");
+    mostrar_ocultar_info(subtitulos_info_2, icono_info_2);
+}
+
+let boton_info_3 = document.getElementById("boton_info_3");
+boton_info_3.addEventListener("click", mostrar_ocultar_3);
+function mostrar_ocultar_3() {
+    let subtitulos_info_3 = document.getElementById("subtitulos_info_3");
+    let icono_info_3 =document.getElementById("icono_info_3");
+    mostrar_ocultar_info(subtitulos_info_3, icono_info_3);
+}
+
+let boton_info_4 = document.getElementById("boton_info_4");
+boton_info_4.addEventListener("click", mostrar_ocultar_4);
+function mostrar_ocultar_4() {
+    let subtitulos_info_4 = document.getElementById("subtitulos_info_4");
+    let icono_info_4 =document.getElementById("icono_info_4");
+    mostrar_ocultar_info(subtitulos_info_4, icono_info_4);
+}
+
+// ----- AVISO EN CONSTRUCCIÓN -----
+let link_plantilla = document.getElementById("link_plantilla");
+link_plantilla.addEventListener("click", aviso);
+function aviso() {
+    if (alert("Esta función aún está en desarrollo, en cambio se le redirigirá al repositorio en GitHub.")) {
+        window.open("https://github.com/Andeath1/CVProyecto");
+    }
+}
+
+// ----- LINK A GITHUB -----
+let boton_github = document.getElementById("boton_github");
+boton_github.addEventListener("click", redirección);
+function redirección() {
+    if (confirm("¿Desea ver el repositorio en GitHub?")) {
+        window.open("https://github.com/Andeath1/CVProyecto");
+    }
+}
+
+// ----- CAMBIO DE TEMA -----
+// Evento click en el botón de temas, dispara las funciones
+let boton_tema = document.getElementById("boton_tema");
 boton_tema.addEventListener("click", cambiar_tema);
-// selecciona y guarda un tema específico
+
+// Selecciona y guarda un tema específico
 function seleccion_tema(nombre_tema) {
     localStorage.setItem('tema', nombre_tema);
     document.documentElement.className = nombre_tema;
 }
-// cambia entre los dos temas (junto al logo)
+
+// Cambia entre los dos temas
 function cambiar_tema() {
     if (localStorage.getItem('tema') === 'tema_oscuro') {
         seleccion_tema('tema_claro');
@@ -55,7 +132,8 @@ function cambiar_tema() {
         seleccion_tema('tema_oscuro');
     }
 }
-// carga el tema que se guardó y el logo correspondiente
+
+// Carga el tema que se guardó y el logo correspondiente
 (function () {
     if (localStorage.getItem('tema') === 'tema_oscuro') {
         seleccion_tema('tema_oscuro');
@@ -63,42 +141,3 @@ function cambiar_tema() {
         seleccion_tema('tema_claro');
     }
 })();
-
-// redirección a GitHub
-function redirección() {
-    if (confirm("¿Desea ver el repositorio en GitHub?")){
-        window.open("https://github.com/Andeath1/CVProyecto", "Gracias, ahora se le redirigirá.");
-    }
-}
-// despliega el aviso
-let boton_plantilla = document.getElementById("plantilla");
-boton_plantilla.addEventListener("click", redirección);
-
-
-// traductor de Google
-function googleTranslateElementInit() {
-    new google.translate.TranslateElement({pageLanguage: 'es', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element')
-}
-
-// Insertar foto dentro del módulo
-let modulo = document.getElementById("modulo");
-let foto = document.getElementById("foto");
-let foto_modulo = document.getElementById("foto_grande");
-foto.addEventListener("click", foto_grande);
-
-function foto_grande() {
-  modulo.style.display = "block";
-  foto_modulo.src = this.src;
-}
-// Botón para cerrar y cerrado por click afuera de la foto
-let x = document.getElementsByClassName("cerrar")[0];
-x.addEventListener("click", cerrar);
-function cerrar() {
-    modulo.style.display = "none";
-}
-window.addEventListener("click", cerrar2);
-function cerrar2 (evento) {
-    if (evento.target == modulo) {
-      modulo.style.display = "none";
-    }
-}
